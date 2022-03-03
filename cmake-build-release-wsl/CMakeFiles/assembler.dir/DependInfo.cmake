b2f07e1d2192b4db8f3157ec1c696d0d138ab4aa
jscudMainDevice <jscudweiss@gmail.com>
2022-03-03 11:11:23 -0500

    begin assembler

---
+++ cmake-build-release-wsl/CMakeFiles/assembler.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/jscud/CLionProjects/AssemblerPT1/assembler.c" "CMakeFiles/assembler.dir/assembler.c.o" "gcc" "CMakeFiles/assembler.dir/assembler.c.o.d"
+  "/mnt/c/Users/jscud/CLionProjects/AssemblerPT1/map.c" "CMakeFiles/assembler.dir/map.c.o" "gcc" "CMakeFiles/assembler.dir/map.c.o.d"
+  "/mnt/c/Users/jscud/CLionProjects/AssemblerPT1/maptest.c" "CMakeFiles/assembler.dir/maptest.c.o" "gcc" "CMakeFiles/assembler.dir/maptest.c.o.d"
+  "/mnt/c/Users/jscud/CLionProjects/AssemblerPT1/mystrcopy.c" "CMakeFiles/assembler.dir/mystrcopy.c.o" "gcc" "CMakeFiles/assembler.dir/mystrcopy.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release-wsl/CMakeFiles/assembler.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assembler.dir/assembler.c.o"
+  "CMakeFiles/assembler.dir/assembler.c.o.d"
+  "CMakeFiles/assembler.dir/map.c.o"
+  "CMakeFiles/assembler.dir/map.c.o.d"
+  "CMakeFiles/assembler.dir/maptest.c.o"
+  "CMakeFiles/assembler.dir/maptest.c.o.d"
+  "CMakeFiles/assembler.dir/mystrcopy.c.o"
+  "CMakeFiles/assembler.dir/mystrcopy.c.o.d"
+  "assembler"
+  "assembler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assembler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
